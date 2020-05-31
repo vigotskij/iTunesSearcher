bdeac14109292faa85232cca91f7cf9266b1e389
vigotskij <boris.sortino@gmail.com>
2020-05-31 01:38:48 -0300

    MainView: Created Main view basic structure.

--- iTunesSearcher/Screens/MainScreen/MainPresenterImplementation.swift
+++ iTunesSearcher/Screens/MainScreen/MainPresenterImplementation.swift
@@ -5,5 +5,14 @@
 //  Created by Boris Sortino on 10/05/2020.
 //  Copyright © 2020 Boris Sortino. All rights reserved.
 //
-final class MainPresenterImplementation {}
-extension MainInteractorImplementation: MainPresenter {}
+final class MainPresenterImplementation {
+    private var output: MainView?
+    init(output: MainView) {
+        self.output = output
+    }
+}
+extension MainPresenterImplementation: MainPresenter {
+    func updatePresentedState(with data: [MainModels.DataModel]) {}
+
+    func routeToDetailScreen() {}
+}
