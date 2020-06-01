e945d06771ddd2d8f95e89666865eca89e2ef12c
vigotskij <boris.sortino@gmail.com>
2020-05-31 22:48:19 -0300

    DetailView: Created response, data and view models for Detail.

--- iTunesSearcher/SupportingObjects/ViewModels/DetailModels.swift
+++ iTunesSearcher/SupportingObjects/ViewModels/DetailModels.swift
@@ -5,12 +5,39 @@
 //  Created by Boris Sortino on 13/05/2020.
 //  Copyright © 2020 Boris Sortino. All rights reserved.
 //
-
+import UIKit
 enum DetailModels {
     // MARK: - Raw data for Interactor
-    struct Response {}
+    struct Response {
+        var rawData: [ITunesResponseDataModel]
+    }
     // MARK: - Interactor processed data for Presenter
-    struct DataModel {}
+    struct DataModel {
+        let coverImageUrl: URL?
+        let albumTitle: String?
+        let filteredData: [SongsDataModel]
+
+        struct SongsDataModel {
+            let trackNumber: Int?
+            let trackName: String?
+        }
+
+        init(from response: Response) {
+            let coverImageString = response.rawData.first?.artworkUrl100 ?? ""
+            albumTitle = response.rawData.first?.collectionName
+            coverImageUrl = URL(string: coverImageString)
+            filteredData = response.rawData.map { SongsDataModel(trackNumber: $0.trackNumber,
+                                                                 trackName: $0.trackName)}
+        }
+    }
     // MARK: - Modeled data for pasive view
-    struct ViewModel {}
+    struct ViewModel {
+        let coverImage: UIImage?
+        let title: String
+        let songsList: [SongsViewModel]
+
+        struct SongsViewModel {
+            let name: String
+        }
+    }
 }
